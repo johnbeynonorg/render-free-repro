81a630f47e32db3db001946f60dc8c42d744ca4d
John Beynon <john@render.com>
2021-11-01 14:35:10 +0000

    health

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'health/index'
+  get '/health', to: 'health#index'
   root to: "posts#index"
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
